d527b80fa24028b28f5958d335830d83eb2f96d3
SHAKIB KHAN <shakib73076@gmail.com>
2026-07-28 09:01:08 +0530

    Time: 87 ms (35.38%), Space: 53.9 MB (98.46%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-   
     string smallestPalindrome(string s) {
-        int n=s.size();
-        string ans3="";
-        if(n%2!=0){
-            ans3=s[n/2];
-        }
-        string  ans="";
-        string ans1="";
-        for(int i=0;i<n/2;i++){
-             ans+=s[i];
-             ans1+=s[n-i-1];
-        }
-         sort(ans.begin(),ans.end());
-         sort(ans1.begin(),ans1.end());
-        reverse(ans1.begin(),ans1.end());
-         if(n%2!=0) return ans+ans3+ans1;
-       return ans+ans1;
+       int n=s.size();
+       int mid=n/2;
+       sort(s.begin(),s.begin()+mid);
+    for(int i=0;i<mid;i++){
+        s[n-i-1]=s[i];
+    }
+      return s;
     }
 };
