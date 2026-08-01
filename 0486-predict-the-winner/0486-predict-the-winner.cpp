88bb889063b83fc8d69ca3af172cde753b991fde
SHAKIB KHAN <shakib73076@gmail.com>
2026-08-02 00:34:43 +0530

    Time: 0 ms (100%), Space: 9.4 MB (96.48%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -5,20 +5,14 @@ int t[20][20];
         if(i>j) return 0;
         if(i==j) return nums[i];
         if(t[i][j]!=-1) return t[i][j];
-        int t1=nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-        int t2=nums[j]+min(solve(i+1,j-1,nums),solve(i,j-2,nums));
+        int t1=nums[i]-solve(i+1,j,nums);
+        int t2=nums[j]-solve(i,j-1,nums);
         return t[i][j]=max(t1,t2);
 
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
         memset(t,-1,sizeof(t));
-       int sum=0;
-        for(int i=0;i<n;i++){
-             sum+=nums[i];
-        }
-      int t1=solve(0,n-1,nums);
-      int t2=sum-t1;
-        return t1>=t2;
+        return solve(0,n-1,nums)>=0;
     }
 };
