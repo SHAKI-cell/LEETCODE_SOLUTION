e1d69b3480893b3973d56828cdd2b4a1391d8c0e
SHAKIB KHAN <shakib73076@gmail.com>
2026-09-02 11:19:11 +0530

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // int n=nums1.size();
+        // vector<int>ans(n);
+        // int eve=0;
+        // int odd=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums1[i]%2==0){
+        //         eve++;
+        //     } else{
+        //         odd++;
+        //     }
+        // }
+        // if(odd==0) return true;
+        // else if(eve==0) return true;
+        // else if(eve!=odd){
+        //     for(int i=0;i<n-1;i++){
+        //       ans[i]=nums1[i]-nums1[i+1];
+        //    if(ans[i]%2==0){
+        //        if(nums1[i+1]%2==0){
+        //            ans[i+1]=nums1[i+1];
+        //        } else{
+        //           ans[i+1]=nums1[i+1]-nums1[i+2];
+        //        }
+        //    } else{
+        //       if(nums1[i+1]%2==0){
+        //           ans[i+1]=nums1[i+1]-nums1[i+2];
+        //       } else{
+        //           ans[i+1]=nums1[i+1];
+        //       }
+        //    }
+        // }
+        // } else if(eve==odd) return true;
+        // // for(int i=0;i<n;i++){
+        // //    if(n>=2) ans[i]=nums1[i]-nums1[i+1];
+        // //    if(ans[i]%2==0){
+               
+        // //    }
+        // // }
+        // return false;
+        return true;
+    }
+};
