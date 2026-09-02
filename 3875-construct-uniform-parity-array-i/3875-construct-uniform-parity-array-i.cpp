1d36e0cf2cb93c9de9eb79c9714e333ea95fd6da
SHAKIB KHAN <shakib73076@gmail.com>
2026-09-02 11:24:13 +0530

    Time: 3 ms (5.87%), Space: 31.1 MB (7.23%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        // int n=nums1.size();
-        // vector<int>ans(n);
-        // int eve=0;
-        // int odd=0;
-        // for(int i=0;i<n;i++){
-        //     if(nums1[i]%2==0){
-        //         eve++;
-        //     } else{
-        //         odd++;
-        //     }
-        // }
+        int n=nums1.size();
+        vector<int>ans(n);
+        int eve=0;
+        int odd=0;
+        for(int i=0;i<n;i++){
+            if(nums1[i]%2==0){
+                eve++;
+            } else{
+                odd++;
+            }
+        }
         // if(odd==0) return true;
         // else if(eve==0) return true;
         // else if(eve!=odd){
@@ -38,7 +38,6 @@ public:
                
         // //    }
         // // }
-        // return false;
         return true;
     }
 };
