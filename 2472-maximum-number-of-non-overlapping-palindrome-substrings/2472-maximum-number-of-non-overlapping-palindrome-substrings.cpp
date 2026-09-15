271afc16ba58390587c957cedfdd38f20eb11ac4
SHAKIB KHAN <shakib73076@gmail.com>
2026-09-16 02:32:06 +0530

    Time: 1873 ms (5.61%), Space: 208.4 MB (17.09%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -26,7 +26,21 @@ public:
      }
     int maxPalindromes(string s, int k) {
         int n=s.size();
-        vector<vector<int>>t(n+1,vector<int>(n+1,-1));
-        return solve(s,k,0,k-1,t);
+        if(k==1) return n;
+        vector<vector<int>>t(n+1,vector<int>(n+1));
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(ispali(s,i,j)){
+                    int take=1+(j+k <=n  ? t[j+1][j+k]:0);
+                    int grow=t[i][j+1];
+                    int slide=t[i+1][j+1];
+                    t[i][j]=max({take,grow,slide});
+                }
+                int grow=t[i][j+1];
+                int slide=t[i+1][j+1];
+              t[i][j]=max({t[i][j],grow,slide});
+            }
+        }
+        return t[0][k-1];
     }
 };
